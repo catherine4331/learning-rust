3ee706d19d0aae33ffea36012c632a6bc7f812db
catherine4331 <catherine4331@gmail.com>
2024-02-19 21:56:15 +0000

    Bit of lovely refactoring

--- lilgrep/src/main.rs
+++ lilgrep/src/main.rs
@@ -1,15 +1,29 @@
+#![warn(clippy::all)]
+
 use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config: Config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    Config {
+        query: args[1].clone(),
+        file_path: args[2].clone(),
+    }
 }
