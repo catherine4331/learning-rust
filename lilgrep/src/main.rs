26c7bbd2c8be4cf2cbfc110debf59a6b0bdba353
catherine4331 <catherine4331@gmail.com>
2024-02-20 10:21:13 +0000

    Move into lib.rs

--- lilgrep/src/main.rs
+++ lilgrep/src/main.rs
@@ -1,8 +1,9 @@
 #![warn(clippy::all)]
 
-use std::error::Error;
+use std::env;
 use std::process;
-use std::{env, fs};
+
+use lilgrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,33 +16,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = lilgrep::run(config) {
         println!("Application error: {e}")
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        Ok(Self {
-            query: args[1].clone(),
-            file_path: args[2].clone(),
-        })
     }
 }
