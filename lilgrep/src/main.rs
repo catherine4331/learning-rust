40573a048153c8cdb707918e1a26901e8cec9290
catherine4331 <catherine4331@gmail.com>
2024-02-20 10:16:53 +0000

    Add some error handling

--- lilgrep/src/main.rs
+++ lilgrep/src/main.rs
@@ -1,19 +1,31 @@
 #![warn(clippy::all)]
 
+use std::error::Error;
+use std::process;
 use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config: Config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}")
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
@@ -21,9 +33,15 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    Config {
-        query: args[1].clone(),
-        file_path: args[2].clone(),
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        Ok(Self {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
     }
 }
