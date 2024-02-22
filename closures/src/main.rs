e58051dcb04c9971bf63b1de0a80695e56400f65
catherine4331 <catherine4331@gmail.com>
2024-02-22 15:59:12 +0000

    Add more closure examples

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,10 +1,55 @@
+use std::thread;
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
+    // Mutable borrowing via a closure
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let mut borrows_mutably = || list.push(7);
+
+    borrows_mutably();
+    println!("After calling closure: {:?}", list);
+
+    // Moving ownership into the closure
     let list = vec![1, 2, 3];
     println!("Before defining closure: {:?}", list);
 
-    let only_borrows = || println!("From closure: {:?}", list);
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
 
-    println!("Before calling closure: {:?}", list);
-    only_borrows();
-    println!("After calling closure: {:?}", list);
+    // Even more fun with closures
+    let mut list = [
+        Rectangle {
+            width: 10,
+            height: 1,
+        },
+        Rectangle {
+            width: 3,
+            height: 5,
+        },
+        Rectangle {
+            width: 7,
+            height: 12,
+        },
+    ];
+
+    // This works
+    list.sort_by_key(|r| r.width);
+    println!("{:#?}", list);
+
+    let mut sort_operations: Vec<String> = vec![];
+    let value = String::from("by key called");
+
+    list.sort_by_key(|r| {
+        // This doesn't because we're moving `value` out of the closure environment which we can't do in an `FnMut`
+        // sort_operations.push(value);
+        r.width
+    });
 }
